c881fddcb4e11c10ebbab974bd21d902f9f259c7
Favourlisticc <favoursunday600@gmail.com>
2025-09-24 10:32:38 +0100

    updating logo

--- src/components/HealthPackages.tsx
+++ src/components/HealthPackages.tsx
@@ -540,7 +540,7 @@ export default function HealthPackages() {
           {/* Call to Action Buttons */}
           <div className="bg-gray-50 px-4 sm:px-2 py-4 flex flex-col sm:flex-row sm:justify-between sm:items-center">
             <motion.div 
-              className="flex items-center mb-4 sm:mb-0"
+              className="flex items-center justify-center mb-4 sm:mb-0"
               initial={{ opacity: 0, x: -20 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ delay: 2.2, duration: 0.5 }}
@@ -591,7 +591,7 @@ export default function HealthPackages() {
           </div>
           
           <motion.div 
-            className="bg-white px-4 sm:px-6 py-4 flex flex-col sm:flex-row sm:justify-between sm:items-center border-t border-gray-200"
+            className="bg-white max-sm:text-center px-2 sm:px-6 py-4 flex flex-col sm:flex-row sm:justify-between sm:items-center border-t border-gray-200"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 2.4, duration: 0.5 }}
@@ -599,7 +599,7 @@ export default function HealthPackages() {
             <div className="text-gray-700 text-sm sm:text-base mb-2 sm:mb-0">
               <span className="font-medium">Visit Us:</span> 🏢 40 Providence St, Lekki Phase 1, Lagos 📍
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center justify-center">
               <span className="mr-2 text-gray-700 text-sm sm:text-base">Book Online</span>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd" />
